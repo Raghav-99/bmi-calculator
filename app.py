22d46b7f5e263d5ccf99c547d6c1ee5ea6705a78
Raghav-99 <raghavmewar2000@gmail.com>
2022-08-07 21:44:39 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from tkinter import E
+from flask import Flask, request, render_template
+from decimal import Decimal
+
+app = Flask(__name__)
+@app.route('/', methods = ['GET', 'POST'])
+
+def index():
+    height = 0
+    weight = 0.000
+    bmi = ''
+    if request.method == 'POST' and 'height' and 'weight' in request.form:
+        height = float(request.form.get('height'))/100
+        weight = float(request.form.get('weight'))
+        
+        if(height == 0):
+            raise ZeroDivisionError("height must not be zero!")
+        elif(height < 0 or weight < 0):
+            raise ValueError("height and weight must be non-negative!")
+        
+        bmi = calc_bmi(weight, height)
+    return render_template('index.html', result=bmi,)
+
+def calc_bmi(weight, height):
+    bmi = round(weight/height**2, 2)  # *calc BMI and limit the value to 2 decimal places
+    return bmi
